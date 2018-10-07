af4230180ad298f9e85246d554929eef6fc50518
siudeks <slawomir@siudek.net>
2018-10-07 23:25:13 +0200

    Fix comments, remove commented code

--- MonoGame.Framework/Graphics/ModelBone.cs
+++ MonoGame.Framework/Graphics/ModelBone.cs
@@ -1,13 +1,12 @@
-﻿using Microsoft.Xna.Framework;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Microsoft.Xna.Framework.Graphics
 {
-	// Summary:
-	//     Represents bone data for a model. Reference page contains links to related
-	//     conceptual articles.
-	public sealed class ModelBone
+    /// <summary>
+    /// Represents bone data for a model. Reference page contains links to related
+    /// conceptual articles.
+    /// </summary>
+    public sealed class ModelBone
 	{
 		private List<ModelBone> children = new List<ModelBone>();
 		
@@ -22,26 +21,35 @@ namespace Microsoft.Xna.Framework.Graphics
 			}
 		}
 
-		// Summary:
-		//     Gets a collection of bones that are children of this bone.
-		public ModelBoneCollection Children { get; private set; }
-		//
-		// Summary:
-		//     Gets the index of this bone in the Bones collection.
-		public int Index { get; set; }
-		//
-		// Summary:
-		//     Gets the name of this bone.
+        /// <summary>
+        /// Gets a collection of bones that are children of this bone.
+        /// </summary>
+        public ModelBoneCollection Children { get; private set; }
+
+        /// <summary>
+        /// Gets the index of this bone in the Bones collection.
+        /// </summary>
+        public int Index { get; set; }
+
+        /// <summary>
+        /// Gets the name of this bone.
+        /// </summary>
 		public string Name { get; set; }
-		//
-		// Summary:
-		//     Gets the parent of this bone.
-		public ModelBone Parent { get; set; }
-		//
-		// Summary:
-		//     Gets or sets the matrix used to transform this bone relative to its parent
-		//     bone.
-		internal Matrix transform;
+
+        /// <summary>
+        /// Gets the parent of this bone.
+        /// </summary>
+        public ModelBone Parent { get; set; }
+
+        //
+        // Summary:
+        //
+        /// <summary>
+        /// Gets or sets the matrix used to transform this bone relative to
+        /// its parent bone.
+        /// </summary>
+        internal Matrix transform;
+
 		public Matrix Transform 
 		{ 
 			get { return this.transform; } 
@@ -72,31 +80,4 @@ namespace Microsoft.Xna.Framework.Graphics
 			Children = new ModelBoneCollection(children);
 		}
 	}
-
-	//// Summary:
-	////     Represents bone data for a model. Reference page contains links to related
-	////     conceptual articles.
-	//public sealed class ModelBone
-	//{
-	//    // Summary:
-	//    //     Gets a collection of bones that are children of this bone.
-	//    public ModelBoneCollection Children { get { throw new NotImplementedException(); } }
-	//    //
-	//    // Summary:
-	//    //     Gets the index of this bone in the Bones collection.
-	//    public int Index { get { throw new NotImplementedException(); } }
-	//    //
-	//    // Summary:
-	//    //     Gets the name of this bone.
-	//    public string Name { get { throw new NotImplementedException(); } }
-	//    //
-	//    // Summary:
-	//    //     Gets the parent of this bone.
-	//    public ModelBone Parent { get { throw new NotImplementedException(); } }
-	//    //
-	//    // Summary:
-	//    //     Gets or sets the matrix used to transform this bone relative to its parent
-	//    //     bone.
-	//    public Matrix Transform { get { throw new NotImplementedException(); } set { throw new NotImplementedException(); } }
-	//}
 }
